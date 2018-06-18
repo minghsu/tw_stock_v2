4bb555aa941d8fec153eff348bdbbe432a880525
Hsu Chih-Ming <hsu.chihming@gmail.com>
2018-06-18 20:30:52 +0800

    - remove unnecessary ANSI code.

--- viewer.py
+++ viewer.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-DICT_TEXT_STYLE = {
-    "RESET": "\33[0m",
-    "BOLD": "\33[1m",
-    "RED": "\33[31m",
-    "CLEAR_SCREEN": "\033[2J\033[H",
-    "CURSOR_UP": "\033[%sA"
-}
-
-from colorama import init, Fore, Back, Style, Cursor
-init()
+import colorama
+colorama.init()
 
 
 def string(arg_string):
@@ -18,13 +10,11 @@ def string(arg_string):
 
 
 def bold_string(arg_string):
-    # print(DICT_TEXT_STYLE['BOLD'] + arg_string + DICT_TEXT_STYLE['RESET'])
-    print(Style.BRIGHT + arg_string + Style.NORMAL)
+    print(colorama.Style.BRIGHT + arg_string + colorama.Style.NORMAL)
 
 
 def warning_string(arg_string):
-    #print(DICT_TEXT_STYLE['RED'] + arg_string + DICT_TEXT_STYLE['RESET'])
-    print(Fore.RED + arg_string + Fore.RESET)
+    print(colorama.Fore.RED + arg_string + colorama.Fore.RESET)
 
 
 def empty_string():
@@ -32,5 +22,4 @@ def empty_string():
 
 
 def move_cursor_up(arg_line=1):
-    #print(DICT_TEXT_STYLE['CURSOR_UP'] % (argLine), end='', flush=True)
-    print(Cursor.UP(arg_line), end='', flush=True)
+    print(colorama.Cursor.UP(arg_line), end='', flush=True)
