9a1c388a877c403bc45bc7228dfe0ee8869a6a26
Hsu Chih-Ming <hsu.chihming@gmail.com>
2018-06-18 20:22:25 +0800

    - use "colorama" module for color/style output

--- viewer.py
+++ viewer.py
@@ -9,26 +9,28 @@ DICT_TEXT_STYLE = {
     "CURSOR_UP": "\033[%sA"
 }
 
+from colorama import init, Fore, Back, Style, Cursor
+init()
+
 
 def string(arg_string):
     print(arg_string)
 
 
 def bold_string(arg_string):
-    print(DICT_TEXT_STYLE['BOLD'] + arg_string + DICT_TEXT_STYLE['RESET'])
+    # print(DICT_TEXT_STYLE['BOLD'] + arg_string + DICT_TEXT_STYLE['RESET'])
+    print(Style.BRIGHT + arg_string + Style.NORMAL)
 
 
 def warning_string(arg_string):
-    print(DICT_TEXT_STYLE['RED'] + arg_string + DICT_TEXT_STYLE['RESET'])
+    #print(DICT_TEXT_STYLE['RED'] + arg_string + DICT_TEXT_STYLE['RESET'])
+    print(Fore.RED + arg_string + Fore.RESET)
 
 
 def empty_string():
     print("")
 
 
-def clear_screen():
-    print(DICT_TEXT_STYLE['CLEAR_SCREEN'], end='', flush=True)
-
-
-def move_cursor_up(argLine=1):
-    print(DICT_TEXT_STYLE['CURSOR_UP'] % (argLine), end='', flush=True)
+def move_cursor_up(arg_line=1):
+    #print(DICT_TEXT_STYLE['CURSOR_UP'] % (argLine), end='', flush=True)
+    print(Cursor.UP(arg_line), end='', flush=True)
